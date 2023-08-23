29e64d403c0b711d191d153bb025eb0c89804380
Bonfim Justino <bonfim1999@gmail.com>
2023-08-23 16:00:32 -0300

    refac: clean desafio 01

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  // Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
-// import { UpdateRouteDto } from './dto/update-route.dto';
 import { RouterSerializar } from './route.serializer';
 
 @Controller('routes')
@@ -25,21 +16,5 @@ export class RoutesController {
   async findAll() {
     const routes = await this.routesService.findAll();
     return routes.map((route) => new RouterSerializar(route));
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const route = await this.routesService.findOne(id);
-    return new RouterSerializar(route);
-  }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
-  //   return this.routesService.update(+id, updateRouteDto);
-  //}
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.routesService.remove(id);
   }
 }
